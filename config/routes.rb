d4d3c074870690eca86e45dd2b2e6dbcb2c530fd
yamazaki8934 <jdmvgaj@gmail.com>
2019-05-18 17:14:42 +0900

    create Person Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
 		namespace :v1 do
-      resources :people, only: :index, :create
-      resources :photos, only: :index, :create 
+      resources :people, only: [:index, :create]
+      resources :photos, only: [:index, :create] 
     end
   end
 end
